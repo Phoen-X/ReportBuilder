01a89c8cb099310fa3863104a3b2a576a303527c
Phoen-X <Phoen-X@ipnet.ua>
2012-11-23 12:03:32 +0200

    Code refactoring

--- tests/ua/sitronics/Report/Tests/ExcelReportTest.java
+++ tests/ua/sitronics/Report/Tests/ExcelReportTest.java
@@ -16,74 +16,80 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Admin
- * Date: 19.11.12
- * Time: 10:44
+ * Created by IntelliJ IDEA. User: Admin Date: 19.11.12 Time: 10:44
  */
 public class ExcelReportTest
 {
-    @Test
-    public void testCreate() throws Exception
-    {
-        System.out.println("========= TESTING EXCEL REPORT GENERATION ======== ");
-        LinkedHashMap<String, String> map = new LinkedHashMap<String, String>(2);
+	@Test
+	public void testCreate() throws Exception
+	{
+		System.out.println("========= TESTING EXCEL REPORT GENERATION ======== ");
+		LinkedHashMap<String, String> map = new LinkedHashMap<String, String>(2);
 
 
-        String[] headers = {"целое число", "булевое значение", "String Field"};
-        String[] fields  = {"intField",    "boolField",        "stringField"};
+		String[] headers = {"целое число", "булевое значение", "String Field"};
+		String[] fields = {"intField", "boolField", "stringField"};
 
-        //заполняем таблцу соответствия
-        for (int i = 0; i < headers.length; i++)
-        {
-            map.put(fields[i], headers[i]);
-        }
+		//заполняем таблцу соответствия
+		for (int i = 0; i < headers.length; i++)
+		{
+			map.put(fields[i], headers[i]);
+		}
 
-        ExcelReport rep = (ExcelReport) ReportBuilder.getReport(ReportFormat.XLS, map);
-        // тестовые данные
-        ArrayList<GetterTestClass> list = new ArrayList<GetterTestClass>();
-        list.add(new GetterTestClass("test1 is test test1 is test test1 is test test1 is test test1 is test test1 is test test1 is test test1 is test test1 is test", 1));
-        list.add(new GetterTestClass("test2", 11));
+		ExcelReport rep = (ExcelReport) ReportBuilder.getReport(ReportFormat.XLS, map);
+		// тестовые данные
+		ArrayList<GetterTestClass> list = new ArrayList<GetterTestClass>();
+		list.add(new GetterTestClass(
+				"test1 is test test1 is test test1 is test test1 is test test1 is test test1 is test test1 is test " +
+				"test1 is test test1 is test",
+				1));
+		list.add(new GetterTestClass("test2", 11));
 
-        File repFile = new File("TestReport.xls");
-        rep.create(repFile, list);
+		File repFile = new File("TestReport.xls");
+		rep.create(repFile, list);
 
-        System.out.println("checking report created");
+		System.out.println("checking report created");
 
-        Assert.assertTrue(repFile.exists());
-        Assert.assertTrue(repFile.isFile());
-        Assert.assertTrue(repFile.canRead());
+		Assert.assertTrue(repFile.exists());
+		Assert.assertTrue(repFile.isFile());
+		Assert.assertTrue(repFile.canRead());
 
-        System.out.println("trying to open report");
+		System.out.println("trying to open report");
 
-        HSSFWorkbook wb = new HSSFWorkbook(new FileInputStream(repFile));
-        HSSFSheet sheet = wb.getSheet("Report");
+		HSSFWorkbook wb = new HSSFWorkbook(new FileInputStream(repFile));
+		HSSFSheet sheet = wb.getSheet("Report");
 
-        Assert.assertTrue(sheet != null);
+		Assert.assertTrue(sheet != null);
 
-        HSSFRow headerRow = sheet.getRow(0);
+		HSSFRow headerRow = sheet.getRow(0);
 
-        Assert.assertTrue(headerRow != null);
+		Assert.assertTrue(headerRow != null);
 
-        int cellId = 0;
-        for (String header : headers)
-        {
-            HSSFCell cell = headerRow.getCell(cellId++);
-            System.out.println("checking title: " + header);
-            Assert.assertEquals(header, cell.getStringCellValue());
-        }
+		int cellId = 0;
+		for (String header : headers)
+		{
+			HSSFCell cell = headerRow.getCell((short) cellId++);
+			System.out.println("checking title: " + header);
+			Assert.assertEquals(header, cell.getStringCellValue());
+		}
 
-        System.out.println("checking data");
-        HSSFRow row = sheet.getRow(1); // 1 обьект
-        Assert.assertEquals("1", row.getCell(0).getStringCellValue());
-        Assert.assertEquals("false", row.getCell(1).getStringCellValue());
-        Assert.assertEquals("test1 is test test1 is test test1 is test test1 is test test1 is test test1 is test test1 is test test1 is test test1 is test", row.getCell(2).getStringCellValue());
+		System.out.println("checking data");
+		HSSFRow row = sheet.getRow(1); // 1 обьект
+		Assert.assertEquals("1", row.getCell((short) 0).getStringCellValue());
+		Assert.assertEquals("false", row.getCell((short) 1).getStringCellValue());
+		Assert.assertEquals("test1 is test test1 is test test1 is test test1 is test " +
+		                    "test1 is test test1 is test test1 is test " +
+		                    "test1 is test test1 is test",
+				row.getCell(
 
-        row = sheet.getRow(2); // 2 обьект
-        Assert.assertEquals("11", row.getCell(0).getStringCellValue());
-        Assert.assertEquals("false", row.getCell(1).getStringCellValue());
-        Assert.assertEquals("test2", row.getCell(2).getStringCellValue());
 
-        repFile.delete();
-    }
+						(short) 2).getStringCellValue());
+
+		row = sheet.getRow(2); // 2 обьект
+		Assert.assertEquals("11", row.getCell((short) 0).getStringCellValue());
+		Assert.assertEquals("false", row.getCell((short) 1).getStringCellValue());
+		Assert.assertEquals("test2", row.getCell((short) 2).getStringCellValue());
+
+		Assert.assertTrue(repFile.delete());
+	}
 }
